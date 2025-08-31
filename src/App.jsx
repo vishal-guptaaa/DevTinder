ad372f39e3f0b0f0b9ad0c7ddb2de6af80fef5e5
Vishal Gupta <guptaaryan1103.com>
2025-09-01 01:46:19 +0530

    Install and setup of Tailwindcss and DaisyUI && Add Navbar to the App.jsx Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
